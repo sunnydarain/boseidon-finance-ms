425e2506b74df12dea19d60e871bd8a3b20d65fb
yangyang <graceful430@163.com>
2021-04-22 19:05:28 +0800

    yy

--- boseidon-fronted/src/views/Main/Home/components/plus.vue
+++ boseidon-fronted/src/views/Main/Home/components/plus.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 .component-container
-    .plus-button-container
+    .plus-button-container(@click="addProof")
+        i.el-icon-plus
+    span.plus-text 新增凭证
 </template>
 
 <script lang="ts">
@@ -10,7 +12,12 @@ export default defineComponent({
     const data = reactive({});
     return { data };
   },
-  methods: {}
+  methods: {
+    // 新增凭证
+    addProof() {
+      alert("新增凭证");
+    }
+  }
 });
 </script>
 
@@ -20,14 +27,28 @@ export default defineComponent({
     height 100%
     box-sizing border-box
     display flex
-    flex-direction row
+    flex-direction column
     justify-content center
     align-items center
     border 1px solid #e5e5e5
     border-radius 8px
     .plus-button-container
-        width 150px
-        height 150px
-        // border 1px solid #20C89F
-        // border-radius 8px
+        width 100px
+        height 100px
+        border 1px solid #20C89F
+        border-radius 8px
+        display flex
+        flex-direction row
+        justify-content center
+        align-items center
+        font-size 50px
+        font-weight bold
+        color #20C89F
+        cursor pointer
+        &:hover
+            background rgba(32,200,159,0.2)
+    .plus-text
+        font-size 16px
+        margin-top 10px
+        font-weight bold
 </style>
